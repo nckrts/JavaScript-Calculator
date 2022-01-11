c5d288f75f780ca3343ade09cf8c336191789861
Nicolas Romancini <96213551+nckrts@users.noreply.github.com>
2022-01-11 12:40:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,60 @@
 document.addEventListener("keydown", function(e) {
-    //console.log(e);
-   //console.log(e.);
-   console.log("key code", e.code);  
-   console.log("key simbol", e.key);
-   var tec=e.key;
+  console.log("key code", e.code);  
+  console.log("key simbol", e.key);
+  var teclaPressionada = e.key;
 
-            if (isNaN(e.key)){
-               console.log(e.key);
-            }
-            if (e.key == '+'){
-                number("+")
-            }
-            if (e.key == '-'){
-                number("-")
-            }
-            if (e.key =='/'){
-                number("/")
-            }
-            if (e.key == '*'){
-                number("X")
-            }
-            if (e.key == '%'){
-                dif("%")
-            }           
-            if (e.key == '√'){
-                dif("√")
-            }
-            if (!isNaN(e.key)){
-                number(e.key.toString())
-                console.log(e.key);
-             }
-             
-            if (e.key == 'Enter'){
-                resultado("resultado")
-                console.log('enter')
-            }
+           if (isNaN(teclaPressionada)){ //if verificando se é numero ou ñ
+                   switch (teclaPressionada) { //switch chamado para validar qual será a função
+                         case '+': number("+");
+                         break;
 
+                         case '-': number("-");
+                         break;
+
+                         case '/': number("/"); 
+                         break;
+
+                         case '*': number("*");
+                         break;
+
+                         case '%': number("%");
+                         break;
+
+                         case '√':  number("√");          
+                         break;
+
+                         case 'Enter': resultado("resultado");
+                         break;
+                                             }
+                                        }
+           else {
+            number(teclaPressionada.toString());
+            console.log(teclaPressionada);
+                }
 });
 
-function number(num)
-{
-    var numero = document.getElementById('display').innerHTML;
-    document.getElementById('display').innerHTML = numero + num;
-}
-function resultado()
-        {
-            var resultado = document.getElementById('display').innerHTML;
-            if(resultado)
-            {
-                document.getElementById('display').innerHTML = eval(resultado); 
-                // contas padrões Ex: =,-,/ e * 
-            }
-            else
-            {
-                document.getElementById('display').innerHTML = "NaN"
-            }
-        }
-        
-function dif(simbolo){
-    var valorNaTela = document.getElementById('display').innerHTML;
-            if (simbolo === '√'){
-               document.getElementById('display').innerHTML = Math.sqrt(valorNaTela); 
-               //calcular raiz
-                                } 
-            else if(simbolo === '%') {
-              document.getElementById('display').innerHTML = valorNaTela / 100; 
-              //calcular a porcentagem 
-                             }
-}
+  function number(num) {
+      var numero = document.getElementById('display').innerHTML;
+      document.getElementById('display').innerHTML = numero + num;
+  }
+  
+  function resultado() {
+      var resultado = document.getElementById('display').innerHTML;
+      // contas padrões Ex: =,-,/ e *
+      if(resultado) {
+          document.getElementById('display').innerHTML = eval(resultado);  
+      } else {
+          document.getElementById('display').innerHTML = "NaN"
+      }
+  }
+    
+  function dif(simbolo) {
+      var valorNaTela = document.getElementById('display').innerHTML;
+      //calcular raiz  
+      if (simbolo === '√') {
+          document.getElementById('display').innerHTML = Math.sqrt(valorNaTela); 
+      //calcular a porcentagem 
+      } else if(simbolo === '%') {
+          document.getElementById('display').innerHTML = valorNaTela / 100; 
+      }
+  }
